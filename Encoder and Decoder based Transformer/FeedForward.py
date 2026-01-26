ba41b058498fd7a522dc11108e2b4d68948a68cd
Prajwol <lamichhaneprajwool@gmail.com>
2026-01-26 21:11:21 +0545

    Implement postion-wise FeedForward network

---
+++ Encoder and Decoder based Transformer/FeedForward.py
@@ -0,0 +1,16 @@
+import torch 
+import torch as nn
+import torch.nn.Functional as F
+
+class FeedForward(nn.Module):
+    def __init__(self,embedded_size,d_ff):
+        super().__init__()
+        self.fc1=nn.Linear(embedded_size,d_ff)
+        self.relu=nn.ReLU()
+        self.fc2=nn.Linear(d_ff,embedded_size)
+
+    def forward(self,x):
+        output=self.fc1(x)
+        output=self.relu(output)
+        output=self.fc2(output)
+        return output
